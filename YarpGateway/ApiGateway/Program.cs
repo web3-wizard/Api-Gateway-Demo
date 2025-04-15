31bd99764cd6bbbee7dac30bd1edf6c05f219160
Rahul Dey <rahuldey9623@gmail.com>
2025-04-15 15:43:15 +0000

    Add rate limiter policy in Yarp gateway

--- YarpGateway/ApiGateway/Program.cs
+++ YarpGateway/ApiGateway/Program.cs
@@ -1,11 +1,28 @@
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var reverseProxyConfig = builder.Configuration.GetSection("ReverseProxy");
 
 builder.Services.AddReverseProxy().LoadFromConfig(reverseProxyConfig);
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.AddPolicy("FixedWindow", httpContext =>
+        RateLimitPartition.GetFixedWindowLimiter(
+            partitionKey: httpContext.Request.Path,
+            factory: _ => new FixedWindowRateLimiterOptions
+            {
+                AutoReplenishment = true,
+                PermitLimit = 5,
+                Window = TimeSpan.FromSeconds(10)
+            }));
+});
+
 var app = builder.Build();
 
 app.MapReverseProxy();
+
+app.UseRateLimiter();
 
 await app.RunAsync();
